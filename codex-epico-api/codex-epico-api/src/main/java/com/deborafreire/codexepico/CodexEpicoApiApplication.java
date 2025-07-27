b6dc1abf6e40ec5efe8e5bd7ceaa829097102959
Débora Freire <deborafreiresoares@outlook.com>
2025-07-26 22:19:20 -0300

    first commit

---
+++ codex-epico-api/codex-epico-api/src/main/java/com/deborafreire/codexepico/CodexEpicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.deborafreire.codexepico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexEpicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodexEpicoApiApplication.class, args);
+	}
+
+}
---
+++ codex-epico-api/codex-epico-api/src/test/java/com/deborafreire/codexepico/CodexEpicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deborafreire.codexepico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexEpicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
