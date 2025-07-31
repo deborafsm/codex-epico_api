69ef82fd2dbca4213f993a9ac63c14f1fb2dc978
Débora Freire <deborafreiresoares@outlook.com>
2025-07-30 21:13:41 -0300

    Create StatusController.java

---
+++ codex-epico-api/codex-epico-api/src/main/java/com/deborafreire/codexepico/controller/StatusController.java
@@ -0,0 +1,12 @@
+package com.deborafreire.codexepico.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @GetMapping("/status")
+    public String status() {
+        return "Codex Online";
+    }
+}
