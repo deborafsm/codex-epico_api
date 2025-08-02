fb7f537fee87aaba954b9d916656fe8368debdb3
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-08-01 23:11:38 -0300

    Atualizar o CriaturaControler.java

--- codex-epico-api/codex-epico-api/src/main/java/com/deborafreire/codexepico/controller/CriaturaControler.java
+++ codex-epico-api/codex-epico-api/src/main/java/com/deborafreire/codexepico/controller/CriaturaControler.java
@@ -10,5 +10,8 @@ public class CriaturaControler {
     public String status() {
         return "Esta ok";
     }
-
+  @GetMapping
+    public List<Criatura> listarTodas() {
+        return criaturaRepository.findAll();
+    }
 }
