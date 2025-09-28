d4ba5a6dbe1a28a52fd758cd754d70281074bdb8
Débora Freire <deborafreiresoares@outlook.com>
2025-09-28 17:48:21 -0300

    Update EntityCards.java

--- codex-epico-api/codex-epico-api/src/main/java/com/deborafreire/codexepico/Entity/EntityCards.java
+++ codex-epico-api/codex-epico-api/src/main/java/com/deborafreire/codexepico/Entity/EntityCards.java
@@ -1,21 +1,53 @@
 package com.deborafreire.codexepico.Entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity 
+@Entity
 public class EntityCards {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     private String poder;
 
- 
+    public String getPoder() {
+        return poder;
+    }
+
+    public void setPoder(String poder) {
+        this.poder = poder;
+    }
 
 }
