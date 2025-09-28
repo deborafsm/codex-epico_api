e7e2355a4881f57488292c39b69cc349f483f779
Débora Freire <deborafreiresoares@outlook.com>
2025-09-28 17:47:38 -0300

    Create EntityCards.java

---
+++ codex-epico-api/codex-epico-api/src/main/java/com/deborafreire/codexepico/Entity/EntityCards.java
@@ -0,0 +1,21 @@
+package com.deborafreire.codexepico.Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity 
+public class EntityCards {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tipo;
+    private String nome;
+    private String descricao;
+    private String poder;
+
+ 
+
+}
